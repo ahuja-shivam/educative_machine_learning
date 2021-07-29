f2cc0d5d71aaa0e3cea0dbd8ba4b174e549a6f40
Shivam Ahuja <ahujas1203@gmail.com>
2021-07-29 23:40:12 +0530

    added upcasting concept

--- 1_numpy_arrays.py
+++ 1_numpy_arrays.py
@@ -3,3 +3,7 @@ import numpy as np
 # create numpy array and dtype
 arr = np.array([[1,4,5], [6,7,8]], dtype=np.float32)
 print(repr(arr))
+
+# upcasting: string > float > int == all string
+arr2 = np.array([3, 5.6, 'shivam'])
+print(repr(arr2))

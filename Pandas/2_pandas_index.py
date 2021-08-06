636018943cf3e720dbdebebe28905d52e816efb0
Shivam Ahuja <ahujas1203@gmail.com>
2021-08-06 11:39:56 +0530

    Done indexing

---
+++ Pandas/2_pandas_index.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+df = pd.DataFrame({'c1': [1, 2], 'c2': [3, 4],
+                   'c3': [5, 6]}, index=['r1', 'r2'])
+
+print(repr(df['c1'])) #this will return a series (single brackets)
+print(repr(df[['c1']])) #this will return a Dataframe 
+
+print(repr(df[['c1','c2']]))
+
+#slicing 
+
+print(repr(df[0:2])) #this will slice the rows can use lables
+#df['r1'] error
+
+#using iloc and loc
+print('{}\n'.format(df.iloc[1])) #access single row by intiger
+print('{}\n'.format(df.loc['r1'])) #access single row by label 
+
+
+
